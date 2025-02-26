6cd49b6c41baba5515d85e55967f3297d8ebeb65
Pratik Nitin Pisal <pisalpratik2004@gmail.com>
2025-02-26 11:39:08 +0530

    minor chnges

--- RakshaSetu/App.js
+++ RakshaSetu/App.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { ThemeProvider } from './src/context/ThemeContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 
 const App = () => {
   return (
-    <ThemeProvider>
+    
       <SafeAreaProvider>
         <NavigationContainer>
           <Routes/>
         </NavigationContainer>
       </SafeAreaProvider>
-    </ThemeProvider>
+    
   );
 };
 
